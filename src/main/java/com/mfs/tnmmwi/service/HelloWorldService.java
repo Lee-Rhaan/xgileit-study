430be9d12c14ba90d8b94b24027f511ff6da7c24
Lee-Rhaan <rhaanking@gmail.com>
2021-12-13 11:58:58 +0200

    Added Service Layer

---
+++ src/main/java/com/mfs/tnmmwi/service/HelloWorldService.java
@@ -0,0 +1,11 @@
+package com.mfs.tnmmwi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String displayUserInput(String request) {
+        return request;
+    }
+}
